d4d960865a1fc63a833390696d326c34385ae461
Juangoez855 <juangoezdj@gmail.com>
2025-11-27 12:55:55 -0500

    feat: desestructuración de props en CabeceraCV

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,5 +1,5 @@
 // src/components/CabeceraCV.jsx
-
+// Props desestructurados correctamente.
 import React from "react";
 
 export default function CabeceraCV({ nombre, cargo, ciudad, contacto, email }) {
