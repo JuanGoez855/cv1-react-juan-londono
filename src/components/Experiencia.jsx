209716718c4b8175dce2ace385ff6495699a7d5a
Juangoez855 <juangoezdj@gmail.com>
2025-11-27 10:37:14 -0500

    feat: componente Experiencia con 4 cargos definidos

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,21 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        <li>
+          <strong>Desarrollador Full Stack</strong> - Navisoft Gaming (2022-2023)
+        </li>
+        <li>
+          <strong>Desarrollador Frontend</strong> - SoftSolutions S.A.S (2020-2022)
+        </li>
+        <li>
+            <strong>Desarrollador de componentes UI</strong> - DevDesign Studios (2024)
+        </li>
+        <li>
+            <strong>Prácticas en desarrollo de software</strong> - TechDev Colombia (2025)
+        </li>
+      </ul>
+    </section>
+  );
+}
