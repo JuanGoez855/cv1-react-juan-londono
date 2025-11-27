fcbf695b02d29ab17332b0577d76998ef15614db
Juangoez855 <juangoezdj@gmail.com>
2025-11-27 10:36:09 -0500

    feat: componente Educacion con formación complementaria

---
+++ src/components/Educacion.jsx
@@ -0,0 +1,12 @@
+export default function Educacion() {
+  return (
+    <section>
+      <h3>Educación</h3>
+      <ul>
+        <li>
+          <strong>Tecnologo en desarrollo de software</strong> - Sena (2025-2027)
+        </li>
+      </ul>
+    </section>
+  );
+}
