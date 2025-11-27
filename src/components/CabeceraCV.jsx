0c4bf0825be908c996c45cf2205ae8417abc6949
Juangoez855 <juangoezdj@gmail.com>
2025-11-27 10:33:55 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,11 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Juan David Londoño Goez</h1>
+      <h2>Tecnólogo en formación en Desarrollo de Software</h2>
+      <h2>Medellin</h2>
+      <h2>+57 313 508 9521</h2>
+      <h2>juangoezdj@gmail.com</h2>
+    </header>
+  )
+}
