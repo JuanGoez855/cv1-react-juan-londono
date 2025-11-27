3fbe7ec33d20899b6416ecfa3009d9ec9a2cdd95
Juangoez855 <juangoezdj@gmail.com>
2025-11-27 10:37:57 -0500

    feat: componente Perfil con descripción personalizada

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,9 @@
+export default function Perfil() {
+  return (
+    <section>
+      <h3>Perfil</h3>
+      <h4>Soy un aprendiz comprometido del SENA con interés en el desarrollo web y la programación. Me caracterizo por mi disposición para aprender, mi responsabilidad y mi capacidad para trabajar en equipo. Busco seguir 
+        fortaleciendo mis habilidades técnicas mientras aporto soluciones prácticas en cada proyecto.</h4>
+    </section>
+  );
+}
